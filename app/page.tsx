3a3bec433e1666a81351aa5b841d0ef13b648140
halchell <halchell1423@gmail.com>
2025-10-14 21:35:15 +0900

    next/imageでトップページの背景画像を最適化

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import styles from './page.module.css';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -7,7 +8,13 @@ export default function Home() {
       <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
       <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
       </div>
-      <img className={styles.bgimg} src="/img-mv.jpg" alt="" />
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+        width={4000}
+        height={1200}
+      />
     </section>
   );
 }
