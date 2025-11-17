f73d4dad5ff29cc33f9c6282c6ae09ed2ed3be5c
halchell <halchell1423@gmail.com>
2025-11-17 22:02:10 +0900

    ページネーションコンポーネントをニュース一覧に追加

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,11 +1,17 @@
 import { getNewsList } from '@/app/_libs/microcms';
 import NewsList from '../_components/NewsList';
 import { NEWS_LIST_LIMIT } from '@/app/_components/_constants';
+import Pagination from '@/app/_components/Pagination';
 
 export default async function Page() {
-  const { contents: news } = await getNewsList({
+  const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT
   });
 
-  return <NewsList news={news} />;
+  return (
+    <>
+      <NewsList news={news} />
+      <Pagination totalCount={totalCount} />
+    </>
+  );
 }
