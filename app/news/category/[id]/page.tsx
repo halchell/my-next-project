5f345210a9b24c77ed4b1ef7fd65a67b38e10455
halchell <halchell1423@gmail.com>
2025-11-18 19:22:58 +0900

    ニュース一覧ページ、カテゴリーのニュース一覧ページにページネーション機能を実装

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
+import Pagination from "@/app/_components/Pagination";
 import { notFound } from "next/navigation";
 import Category from "@/app/_components/Category";
 import { NEWS_LIST_LIMIT } from "@/app/_components/_constants";
@@ -13,7 +14,7 @@ type Props = {
 export default async function Page({ params }: Props) {
   const category = await getCategoryDetail(params.id).catch(notFound);
 
-  const { contents: news } = await getNewsList({
+  const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
     filters: `category[equals]${category.id}`,
 });
@@ -24,6 +25,7 @@ export default async function Page({ params }: Props) {
     <Category category={category} /> の一覧
   </p>
   <NewsList news={news} />
+  <Pagination totalCount={totalCount} basePath={`/news/category/${category.id}`} />
   </>
   );
 }
