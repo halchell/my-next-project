2e891a30f8e0094e9661092261866ffe1a4d4e36
halchell <halchell1423@gmail.com>
2025-11-19 23:03:36 +0900

    検索機能を実装

--- app/_components/SearchField/index.tsx
+++ app/_components/SearchField/index.tsx
@@ -1,9 +1,24 @@
+"use client";
+
 import Image from 'next/image'
 import styles from './index.module.css'
+import { useRouter } from 'next/router'
 
 export default function SearchField() {
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const q = e.currentTarget.elements.namedItem('q');
+    if (q instanceof HTMLInputElement) {
+      const params = new URLSearchParams();
+      params.set('q', q.value.trim());
+      router.push(`/news/search?${params.toString()}`);
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form} >
       <label className={styles.search}>
         <Image src="/search.svg" alt="検索" width={16} height={16} loading="eager" />
         <input
