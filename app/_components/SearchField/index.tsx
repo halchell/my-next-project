e0ae9277d4889ed85caa224a6a6d83748c5d9484
halchell <halchell1423@gmail.com>
2025-11-20 21:06:53 +0900

    SearchFieldに検索キーワードの保持機能を追加

--- app/_components/SearchField/index.tsx
+++ app/_components/SearchField/index.tsx
@@ -2,10 +2,12 @@
 
 import Image from 'next/image'
 import styles from './index.module.css'
-import { useRouter } from 'next/navigation'
+import { Suspense } from "react";
+import { useRouter, useSearchParams } from 'next/navigation'
 
-export default function SearchField() {
+function SearchFieldComponent() {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -18,16 +20,31 @@ export default function SearchField() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className={styles.form} >
+    <form onSubmit={handleSubmit} className={styles.form}>
       <label className={styles.search}>
-        <Image src="/search.svg" alt="検索" width={16} height={16} loading="eager" />
+        <Image
+          src="/search.svg"
+          alt="検索"
+          width={16}
+          height={16}
+          loading="eager"
+        />
         <input
           type="text"
           name="q"
+          defaultValue={searchParams.get('q') ?? undefined}
           placeholder="キーワードを入力"
           className={styles.searchInput}
         />
       </label>
     </form>
-  )
+  );
+}
+
+export default function SearchField() {
+  return (
+    <Suspense>
+      <SearchFieldComponent />
+    </Suspense>
+  );
 }
